26d568d73c7f30e34cf9ee5cd3a3c859df123457
desmoteuss <desmoteuss@gmail.com>
2021-04-08 20:23:59 +0200

    taking screenshot

--- bot_python.py
+++ bot_python.py
@@ -5,4 +5,8 @@ import cv2
 while(True)
 printscreen_pil = ImageGrab.grab (bbox=(0,40,800,640))
 printscreen_numpy= np.array(printscreen_pil.getdata(), dtype='uint8')\
-.reshape ((printscreen.pill))
+.reshape ((printscreen.pil.size[1],printscreen_pil.size[0],3))
+cv.imshow('window',printscreen_numpy)
+if cv2.waitKey(25) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+    break
